215a46a33d7c15be02cc3bb78c714c3218e30e20
ranebof <165827905+nalyvaykod@users.noreply.github.com>
2024-05-15 03:23:12 +0300

    Enemy Spawn Done and installed AI Navigation.

--- Assets/Scripts/EmenyScripts/EnemySpawner.cs
+++ Assets/Scripts/EmenyScripts/EnemySpawner.cs
@@ -1,17 +1,46 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    public GameObject EnemyObjectToSpawn; 
+    public GameObject Player; 
+    List<GameObject> Enemies = new List<GameObject>(); 
 
-    void Update()
+    public void SpawnEnemy()
     {
-        
+        Vector3 spawnPosition;
+
+        do
+        {
+            
+            Vector3 RandomDistance = new Vector3(Random.Range(-1f,1f), 0f, Random.Range(-1f,1f));
+            RandomDistance.Normalize();
+
+            float RangedDistance = Random.Range(10f, 20f);
+            
+            spawnPosition = Player.transform.position + RandomDistance * RangedDistance; 
+            
+            //Debug.Log(spawnPosition);
+
+        }
+        while (IsPositionOccupied(spawnPosition));
+
+
+        GameObject NewEnemy = Instantiate(EnemyObjectToSpawn, spawnPosition, Quaternion.identity);
+        Enemies.Add(NewEnemy); 
     }
 
-    void SpawnEnemyFunction()
+    bool IsPositionOccupied(Vector3 position)
     {
+        foreach (GameObject enemy in Enemies)
+        {
+            if (Vector3.Distance(position, enemy.transform.position) < 1)
+            {
+                return true;
+            }
+        }
 
+        return false;
     }
 }
