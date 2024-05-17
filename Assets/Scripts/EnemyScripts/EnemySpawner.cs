fa97052fe0014d67d5416f703b405351adb001d1
ranebof <165827905+nalyvaykod@users.noreply.github.com>
2024-05-17 15:27:47 +0300

    Finally done nearest object delete

--- Assets/Scripts/EnemyScripts/EnemySpawner.cs
+++ Assets/Scripts/EnemyScripts/EnemySpawner.cs
@@ -1,11 +1,30 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public GameObject EnemyObjectToSpawn; 
-    public GameObject Player; 
-    List<GameObject> Enemies = new List<GameObject>(); 
+    public GameObject EnemyObjectToSpawn;
+    public GameObject Player;
+    [SerializeField] private List<GameObject> Enemies = new List<GameObject>();
+    [SerializeField] private GameObject NearestEnemy = null;
+
+
+    void Start()
+    {
+       Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        CleanUpEnemy();
+        FindNearestEnemy();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RemoveNearestEnemy();
+        }
+    }
 
     public void SpawnEnemy()
     {
@@ -13,34 +32,88 @@ public class EnemySpawner : MonoBehaviour
 
         do
         {
-            
-            Vector3 RandomDistance = new Vector3(Random.Range(-1f,1f), 0f, Random.Range(-1f,1f));
+
+            Vector3 RandomDistance = new Vector3(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f));
             RandomDistance.Normalize();
 
             float RangedDistance = Random.Range(10f, 20f);
-            
-            spawnPosition = Player.transform.position + RandomDistance * RangedDistance; 
-            
-            //Debug.Log(spawnPosition);
+
+            spawnPosition = Player.transform.position + RandomDistance * RangedDistance;
+
 
         }
         while (IsPositionOccupied(spawnPosition));
 
-
         GameObject NewEnemy = Instantiate(EnemyObjectToSpawn, spawnPosition, Quaternion.identity);
-        Enemies.Add(NewEnemy); 
+        Debug.Log(Enemies);
+        Enemies.Add(NewEnemy);
     }
+
+    
 
     bool IsPositionOccupied(Vector3 position)
     {
-        foreach (GameObject enemy in Enemies)
-        {
-            if (Vector3.Distance(position, enemy.transform.position) < 1)
+        foreach (GameObject enemy in Enemies) { 
+            if(enemy != null && Vector3.Distance(position, enemy.transform.position) < 1f)
             {
                 return true;
             }
         }
-
         return false;
     }
+
+    private void FindNearestEnemy()
+    {
+        float distanceToNearEnemy = Mathf.Infinity;
+        NearestEnemy = null;
+
+        foreach (GameObject currentEnemy in Enemies)
+        {
+            if(currentEnemy == null) continue;
+
+            float distanceToCurrentEnemy = Vector3.Distance(Player.transform.position, currentEnemy.transform.position);
+
+            if(distanceToCurrentEnemy < distanceToNearEnemy)
+            {
+                distanceToNearEnemy = distanceToCurrentEnemy;
+                NearestEnemy = currentEnemy;
+            }
+
+            if(NearestEnemy != null)
+            {
+                Debug.Log("Найближчий ворог:" + NearestEnemy.name);
+            }
+        }
+    } 
+    public void RemoveEnemyFunction(GameObject enemy)
+    {
+        if (enemy != null)
+        {
+            Enemies.Remove(enemy);
+            Destroy(enemy);
+        }
+    }
+
+    private void RemoveNearestEnemy ()
+    {
+        if(NearestEnemy != null)
+        {
+            RemoveEnemyFunction(NearestEnemy);
+        }
+    }
+
+    
+    private void CleanUpEnemy()
+    {
+        Enemies.RemoveAll(enemy => enemy = null);
+    }
+
+
+
+
+
+
+
+
+
 }
--- Assets/Scripts/PlayerScripts/NearestObjectDestroy.cs
+++ Assets/Scripts/PlayerScripts/NearestObjectDestroy.cs
@@ -1,23 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+/*using UnityEngine;
 
 public class NearestObjectDestroy : MonoBehaviour
 {
-    public GameObject[] AllEnemies;
-    public GameObject NearestEnemy;
-
-    float distance;
-    float nearestObjectDistance;
+    public GameObject Player;
+    public GameObject NearestEnemy = null;
 
     void Start()
     {
-        AllEnemies = GameObject.FindGameObjectsWithTag("Enemy");
-        
+        Player = GameObject.FindGameObjectWithTag("Player");
     }
 
     void Update()
     {
-        
+        NearestEnemyFucnt();
+
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            
+        }
+    }
+
+    void NearestEnemyFucnt()
+    {
+        float distanceToNearestEnemy = Mathf.Infinity;
+        GameObject[] allEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        foreach (GameObject currentEnemy in allEnemies)
+        {
+            float distanceToCurrentEnemy = Vector3.Distance(Player.transform.position, currentEnemy.transform.position);
+            if (distanceToCurrentEnemy < distanceToNearestEnemy)
+            {
+                distanceToNearestEnemy = distanceToCurrentEnemy;
+                NearestEnemy = currentEnemy;
+            }
+        }
+
+        if (NearestEnemy != null)
+        {
+            Debug.Log("Nearest Enemy: " + NearestEnemy.name);
+        }       
     }
 }
+*/
